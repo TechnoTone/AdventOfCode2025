582b890986723e6380cd40e9c95a5dc68e98d985
Tony Hunt <thunt@findmypast.com>
2025-12-16 14:02:22 +0000

    Day 12 doesn't have a part 2

--- JS/src/day12.test.js
+++ JS/src/day12.test.js
@@ -45,12 +45,3 @@ test("Part 1", () => {
   const input = new Input(12).fromLines().get();
   expect(part1(input)).toBe(528);
 });
-
-test.skip("Part 2 Example", () => {
-  expect(part2(EXAMPLE_INPUT)).toBe(0);
-});
-
-test.skip("Part 2", () => {
-  const input = new Input(12).fromLines().get();
-  expect(part2(input)).toBe(0);
-});
